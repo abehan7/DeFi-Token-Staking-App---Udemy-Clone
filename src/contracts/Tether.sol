fd77c5aeea2d705697d148bd9e1086fb73fd4a86
한준규 <hanjk13262@gmail.com>
2022-03-21 11:32:11 +0900

    feat:> lesson 99

--- src/contracts/Tether.sol
+++ src/contracts/Tether.sol
@@ -16,6 +16,16 @@ contract Tether {
     );
 
     mapping(address => uint256) public balances;
+    // key : value = address : uint256
+    // FIXME:
+    mapping(address => mapping(address => uint256)) public allowance;
+
+    // allowance = { owner : { spender : amount } }
+
+    // key : value = address : (key : value = address : uint256)
+    // allowance[owner][spender] = amount
+
+    // 주소 => 매핑(주소 => 양)
 
     constructor() {
         balances[msg.sender] = totalSupply;
@@ -32,6 +42,43 @@ contract Tether {
         //  add the balance to the new address
         balances[_to] += _value;
         emit Transfer(msg.sender, _to, _value);
+        return true;
+    }
+
+    function approve(address _spender, uint256 _value)
+        public
+        returns (bool success)
+    {
+        // check if the spender is the same as the owner
+        require(_spender != msg.sender);
+        // check if the value is greater or equal for transfer
+        require(balances[msg.sender] >= _value);
+        // set the allowance to the spender
+        allowance[msg.sender][_spender] = _value;
+        // FIXME: msg.sender이랑 spender이 어떤 관계인지 잘 모르겠어
+        emit Approval(msg.sender, _spender, _value);
+        return true;
+    }
+
+    // FIXME:
+    function transferFrom(
+        address _from,
+        address _to,
+        uint256 _value
+    ) public returns (bool success) {
+        // require that the value is greater or equal for transfer
+        require(balances[_from] >= _value);
+        require(allowance[_from][msg.sender] >= _value);
+        // transfer the amount and substract the balance
+        balances[_from] -= _value;
+        //  add the balance to the new address
+        balances[_to] += _value;
+
+        // substract the allowance
+        // third party
+        allowance[_from][msg.sender] -= _value;
+        // allowance[msg.sender][_from] -= _value;
+        emit Transfer(_from, _to, _value);
         return true;
     }
 }
